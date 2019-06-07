21dcc0563c4fe23b137f02284989c235500147b2
gjsravs <49661246+gjsravs@users.noreply.github.com>
2019-06-07 15:33:19 -0400

    Add files via upload

---
+++ Hashset.java
@@ -0,0 +1,21 @@
+package exercises;
+
+import java.util.*;  
+class Hashset{  
+public static void main(String args[]){  
+ //Creating HashSet and adding elements  
+   HashSet<String> set=new HashSet<String>();  
+          set.add("One");    
+          set.add("Two");    
+          set.add("Three");   
+          set.add("Four");  
+          set.add("Five");  
+          Iterator<String> i=set.iterator();  
+          while(i.hasNext())  
+          {  
+          System.out.println(i.next());  
+          }  
+}  
+} 
+
+
